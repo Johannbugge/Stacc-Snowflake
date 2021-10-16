d8b7192940559a1f703dd37bf6305d88bb2f30a6
Fredrik Haarde <32198607+haardes@users.noreply.github.com>
2021-10-16 19:24:37 +0200

    Fetching data from OpenSea API

--- stacc-snowflake/src/App.js
+++ stacc-snowflake/src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-    return <div className='App'></div>;
+	const options = { method: 'GET' };
+	const data = fetch(
+		'https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50&collection=xenogems',
+		options
+	)
+		.then((response) => response.json())
+		.then((response) => console.log(response))
+		.catch((err) => console.error(err));
+
+	return <div className='App'></div>;
 }
 
 export default App;
