44670e0c10d138a9adbdeb9dcabe318cad71d260
Johann Bugge <60435883+Johannbugge@users.noreply.github.com>
2021-10-17 16:49:55 +0200

    Added about

--- stacc-snowflake/src/App.js
+++ stacc-snowflake/src/App.js
@@ -4,55 +4,58 @@ import Navbar from './components/navbar';
 import PieceGrid from './components/pieces/pieceGrid';
 import Hero from './components/hero';
 import ProgressIndicator from './components/progressIndicator';
+import About from './components/about';
 
 function App() {
-	const [data, setData] = useState([]);
-	const [hasMoreData, setHasMoreData] = useState(true);
-	const [offset, setOffset] = useState(0);
-	const [loading, setLoading] = useState(true);
-	const limit = 20;
-	const options = { method: 'GET' };
-	const collectionName = 'xenogems';
+    const [data, setData] = useState([]);
+    const [hasMoreData, setHasMoreData] = useState(true);
+    const [offset, setOffset] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const limit = 20;
+    const options = { method: 'GET' };
+    const collectionName = 'xenogems';
 
-	function handleScroll() {
-		const bottom = window.scrollY + window.innerHeight === document.body.scrollHeight;
-		if (!loading && hasMoreData && bottom) {
-			console.log('Bottom!');
-			setLoading(true);
-			loadData();
-		}
-	}
+    function handleScroll() {
+        const bottom =
+            window.scrollY + window.innerHeight === document.body.scrollHeight;
+        if (!loading && hasMoreData && bottom) {
+            console.log('Bottom!');
+            setLoading(true);
+            loadData();
+        }
+    }
 
-	function loadData() {
-		fetch(
-			`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=${offset}&limit=${limit}&collection=${collectionName}`,
-			options
-		)
-			.then((response) => response.json())
-			.then((response) => {
-				setData([...data, ...response.assets]);
-				setOffset(offset + limit);
-				setLoading(false);
-				if (response.assets.length < limit) setHasMoreData(false);
-			})
-			.catch((err) => console.error(err));
-	}
+    function loadData() {
+        fetch(
+            `https://api.opensea.io/api/v1/assets?order_direction=desc&offset=${offset}&limit=${limit}&collection=${collectionName}`,
+            options
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                setData([...data, ...response.assets]);
+                setOffset(offset + limit);
+                setLoading(false);
+                if (response.assets.length < limit) setHasMoreData(false);
+            })
+            .catch((err) => console.error(err));
+    }
 
-	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	});
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
 
-	return (
-		<div className='App'>
-			<Navbar></Navbar>
-			{data.length === 0 ? loadData() : <Hero piece={data[9]}></Hero>}
-			{data.length === 0 ? null : <PieceGrid pieces={data}></PieceGrid>}
-			{loading ? <ProgressIndicator></ProgressIndicator> : null}
-		</div>
-	);
+    return (
+        <div className='App'>
+            <About></About>
+            <Navbar></Navbar>
+            {data.length === 0 ? loadData() : <Hero piece={data[6]}></Hero>}
+            {data.length === 0 ? null : <PieceGrid pieces={data}></PieceGrid>}
+            {loading ? <ProgressIndicator></ProgressIndicator> : null}
+        </div>
+    );
 }
 
 export default App;
--- stacc-snowflake/src/components/pieces/pieceCard.js
+++ stacc-snowflake/src/components/pieces/pieceCard.js
@@ -3,19 +3,29 @@ import React from 'react';
 import styles from './pieceCard.module.css';
 
 const PieceCard = (props) => {
-	return (
-		<div>
-			<button className={styles.fav}></button>
-			<div className={styles.card}>
-				<img className={styles.cardImg} src={props.piece.image_preview_url} alt={props.piece.description}></img>
-				<p className={styles.name}>{props.piece.name}</p>
-				<p className={styles.desc}>{props.piece.description}</p>
-				{props.piece.sell_orders ? (
-					<p>{parseInt(props.piece.sell_orders[0].current_price) / 1000000000000000000} Ξ</p>
-				) : null}
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <button className={styles.fav}></button>
+            <div className={styles.card}>
+                <img
+                    className={styles.cardImg}
+                    src={props.piece.image_preview_url}
+                    alt={props.piece.description}
+                ></img>
+                <div className={styles.info}>
+                    <p className={styles.name}>{props.piece.name}</p>
+                    <p className={styles.desc}>{props.piece.description}</p>
+                </div>
+                {props.piece.sell_orders ? (
+                    <p className={styles.price}>
+                        {parseInt(props.piece.sell_orders[0].current_price) /
+                            1000000000000000000}{' '}
+                        Ξ
+                    </p>
+                ) : null}
+            </div>
+        </div>
+    );
 };
 
 export default PieceCard;
