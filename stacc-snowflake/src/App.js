ba0089a54de680c9e13cdf4addf2163e96c3371b
Johann Bugge <60435883+Johannbugge@users.noreply.github.com>
2021-10-17 18:22:01 +0200

    yolo

--- stacc-snowflake/src/App.js
+++ stacc-snowflake/src/App.js
@@ -7,54 +7,55 @@ import ProgressIndicator from './components/progressIndicator';
 import About from './components/about';
 
 function App() {
-	const [data, setData] = useState([]);
-	const [hasMoreData, setHasMoreData] = useState(true);
-	const [offset, setOffset] = useState(0);
-	const [loading, setLoading] = useState(true);
-	const limit = 20;
-	const options = { method: 'GET' };
-	const collectionName = 'xenogems';
+    const [data, setData] = useState([]);
+    const [hasMoreData, setHasMoreData] = useState(true);
+    const [offset, setOffset] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const limit = 40;
+    const options = { method: 'GET' };
+    const collectionName = 'xenogems';
 
-	function handleScroll() {
-		const bottom = window.scrollY + window.innerHeight === document.body.scrollHeight;
-		if (!loading && hasMoreData && bottom) {
-			console.log('Bottom!');
-			setLoading(true);
-			loadData();
-		}
-	}
+    function handleScroll() {
+        const bottom =
+            window.scrollY + window.innerHeight === document.body.scrollHeight;
+        if (!loading && hasMoreData && bottom) {
+            console.log('Bottom!');
+            setLoading(true);
+            loadData();
+        }
+    }
 
-	function loadData() {
-		fetch(
-			`https://api.opensea.io/api/v1/assets?order_direction=asc&offset=${offset}&limit=${limit}&collection=${collectionName}`,
-			options
-		)
-			.then((response) => response.json())
-			.then((response) => {
-				setData([...data, ...response.assets]);
-				setOffset(offset + limit);
-				setLoading(false);
-				if (response.assets.length < limit) setHasMoreData(false);
-			})
-			.catch((err) => console.error(err));
-	}
+    function loadData() {
+        fetch(
+            `https://api.opensea.io/api/v1/assets?order_direction=asc&offset=${offset}&limit=${limit}&collection=${collectionName}`,
+            options
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                setData([...data, ...response.assets]);
+                setOffset(offset + limit);
+                setLoading(false);
+                if (response.assets.length < limit) setHasMoreData(false);
+            })
+            .catch((err) => console.error(err));
+    }
 
-	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	});
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
 
-	return (
-		<div className='App'>
-			<About></About>
-			<Navbar></Navbar>
-			{data.length === 0 ? loadData() : <Hero piece={data[38]}></Hero>}
-			{data.length === 0 ? null : <PieceGrid pieces={data}></PieceGrid>}
-			{loading ? <ProgressIndicator></ProgressIndicator> : null}
-		</div>
-	);
+    return (
+        <div className='App'>
+            <About></About>
+            <Navbar></Navbar>
+            {data.length === 0 ? loadData() : <Hero piece={data[38]}></Hero>}
+            {data.length === 0 ? null : <PieceGrid pieces={data}></PieceGrid>}
+            {loading ? <ProgressIndicator></ProgressIndicator> : null}
+        </div>
+    );
 }
 
 export default App;
