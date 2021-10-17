bc00b2ff5a810000a72e868c78a75366898471ce
Fredrik Haarde <32198607+haardes@users.noreply.github.com>
2021-10-17 15:31:59 +0200

    Added infinte scrolling and loading

--- stacc-snowflake/src/App.js
+++ stacc-snowflake/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import PieceGrid from './components/pieces/pieceGrid';
@@ -7,26 +7,50 @@ import ProgressIndicator from './components/progressIndicator';
 
 function App() {
 	const [data, setData] = useState([]);
+	const [hasMoreData, setHasMoreData] = useState(true);
 	const [offset, setOffset] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const limit = 20;
 	const options = { method: 'GET' };
 	const collectionName = 'xenogems';
 
+	function handleScroll() {
+		const bottom = window.scrollY + window.innerHeight === document.body.scrollHeight;
+		if (!loading && hasMoreData && bottom) {
+			console.log('Bottom!');
+			setLoading(true);
+			loadData();
+		}
+	}
+
 	function loadData() {
 		fetch(
-			`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=${offset}&limit=50&collection=${collectionName}`,
+			`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=${offset}&limit=${limit}&collection=${collectionName}`,
 			options
 		)
 			.then((response) => response.json())
-			.then((response) => setData(response.assets))
+			.then((response) => {
+				setData([...data, ...response.assets]);
+				setOffset(offset + limit);
+				setLoading(false);
+				if (response.assets.length < limit) setHasMoreData(false);
+			})
 			.catch((err) => console.error(err));
-		return <ProgressIndicator></ProgressIndicator>;
 	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	});
 
 	return (
 		<div className='App'>
 			<Navbar></Navbar>
 			<Hero></Hero>
 			{data.length === 0 ? loadData() : <PieceGrid pieces={data}></PieceGrid>}
+			{loading ? <ProgressIndicator></ProgressIndicator> : null}
 		</div>
 	);
 }
